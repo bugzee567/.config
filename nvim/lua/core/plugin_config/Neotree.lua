c1a513b65ef55278bc07aea929fb598aee68edca
Bugzee <bugzee@bugzee.com>
2023-11-11 15:39:49 +0200

    initial

---
+++ nvim/lua/core/plugin_config/Neotree.lua
@@ -0,0 +1,5 @@
+require('neo-tree').setup()
+
+
+vim.keymap.set('n', '<leader>e', ':Neotree<CR>')
+vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>')
---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,79 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+  })
+
+  use 'wbthomason/packer.nvim'
+  use 'tpope/vim-commentary'
+  use 'ellisonleao/gruvbox.nvim'
+  use 'dracula/vim'
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use 'nvim-lualine/lualine.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'bluz71/vim-nightfly-colors'
+  use 'vim-test/vim-test'
+  use 'lewis6991/gitsigns.nvim'
+  use 'preservim/vimux'
+  use 'christoomey/vim-tmux-navigator'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-surround'
+  use 'stevearc/oil.nvim'
+  use 'vimwiki/vimwiki'
+  -- completion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use "rafamadriz/friendly-snippets"
+  use "github/copilot.vim"
+
+  use({
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  })
+
+
+use 'jose-elias-alvarez/null-ls.nvim'
+use 'jay-babu/mason-null-ls.nvim'
+  use({
+      "vinnymeller/swagger-preview.nvim",
+      run = "npm install -g swagger-ui-watcher",
+  })
+
+
+use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
+
+  use({
+	  'nvim-telescope/telescope.nvim',
+	  tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  })
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
